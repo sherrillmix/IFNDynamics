1727e3aca05998856afde31799a2a16dce07c64f
sherrillmix <sherrillmix@users.noreply.github.com>
2017-09-24 09:40:14 -0400

    Standardize plots. Plot 5 vars. Catch 0 beta vres. Identify bulks

--- readNewData.R
+++ readNewData.R
@@ -7,13 +7,15 @@ meta<-meta[meta$Time.Points!='Total number of sequences',]
 rownames(meta)<-meta$Time.Points
 
 #Standardize BULK naming and catch _BULK with no .
-dat$sample<-sub('[._][^._]*$','',sub('_Bulk','BULK',sub('_BULK','BULK',sub('\\.([0-9]+)_Bulk','.\\1.BULK',dat$ID.for.Publications))))
+dat$id<-sub('_Bulk|_BULK','-BULK',sub('\\.([0-9]+)_Bulk','.\\1.BULK',dat$ID.for.Publications))
+dat$sample<-sub('[._][^._]*$','',dat$id)
 dat$sample<-sub('\\.([0-9])$','.0\\1',dat$sample)
 dat$sample<-sub('\\.([0-9])\\.','.0\\1.',dat$sample)
 dat$pat<-sub('\\.[^.]+$','',dat$sample)
 dat$time<-meta[dat$sample,'DFOSx']
 dat$vl<-as.numeric(gsub(',','',meta[dat$sample,'VL']))
 dat$CD4<-as.numeric(gsub(',','',meta[dat$sample,'CD4']))
+dat$bulk<-grepl('BULK',dat$id)
 if(any(is.na(dat$vl)))stop('Missing vl')
 if(any(is.na(dat$time)))stop('Missing time')
 if(any(is.na(dat$CD4)))stop('Missing CD4')
@@ -21,6 +23,13 @@ if(any(is.na(dat$CD4)))stop('Missing CD4')
 
 dat$ic50<-apply(dat[,c('IFNa2..Oct.2016..Pooled.Donor.cells.IC50..pg.ml.','REPEAT....June.2017.....IFNa2.Pooled.Donor.cells.IC50..pg.ml.')],1,mean,na.rm=TRUE)
 dat$vres<-apply(dat[,c('IFNa2.....Oct..2016..Pooled.Donor.Vres.at.5.5pg.ml...UT.','REPEAT....June.2017...IFNa2.Pooled.Donor.Vres.at.5.5pg.ml...UT.')],1,mean,na.rm=TRUE)
+dat$beta<-dat$IFNbeta..Pooled.Donor.cells.IC50..pg.ml.
+dat$betaVres<-dat$IFNbeta..Pooled.Donor.Vres.at.4.400.pg.ml...UT.
+if(any(dat$betaVres==0)){
+  warning('Beta Vres equal to zero. Setting to lowest value/10.')
+  dat[dat$betaVres==0&!is.na(dat$betaVres),'betaVres']<-min(dat[dat$betaVres!=0&!is.na(dat$betaVres),'betaVres'])/10
+}
+dat$replication<-dat$Replicative.capacity.Pooled.Donor.cells.p24.d7..from.June.2017.repeat.
 
 patCols<-c('MM23'='#FF0000','MM33'='#00FF00','MM34'='#8000FF','MM39'='#0000FF','MM40'='#FF8000')
 patCols2<-c('MM23'='#FF000033','MM33'='#00FF0033','MM34'='#8000FF33','MM39'='#0000FF33','MM40'='#FF800033')
@@ -60,24 +69,64 @@ pdf('out/time_vs_VL.pdf',width=5,height=5)
   par(mfrow=c(3,2),mar=c(3.6,4.2,1.1,.1))
 dev.off()
 
-pdf('out/time_vs_all.pdf',width=8,height=8)
+plot3vars<-function(var,lab,dat,logX=FALSE){
+  logAdd<-ifelse(logX,'x','')
+  logAddY<-sprintf('%sy',logAdd)
   par(mfrow=c(3,2),mar=c(3.6,4.2,1.1,11))
   for(ii in unique(dat$pat)){
-    withAs(xx=dat[dat$pat==ii,],plot(xx$time/7,xx$ic50,bg=patCols[xx$pat],pch=21,las=1,log='y',yaxt='n',xlab='',ylab='IFNa IC50',main=ii,xlim=range(dat$time/7),ylim=range(dat$ic50,na.rm=TRUE)))
+    withAs(xx=dat[dat$pat==ii,],plot(xx$time/7,xx[,var],bg=patCols[xx$pat],pch=21,las=1,log=logAddY,yaxt='n',xlab='',ylab=lab,main=ii,xlim=range(dat$time/7),ylim=range(dat[,var],na.rm=TRUE)))
     title(xlab='Time (weeks)',mgp=c(2,1,0))
     logAxis(las=1)
     thisDat<-dat[dat$pat==ii,]
     par(new=TRUE)
-    withAs(xx=unique(dat[dat$pat==ii,c('time','vl')]),plot(xx$time/7,xx$vl,type='l',log='y',yaxt='n',xlab='',ylab='',xlim=range(dat$time/7),ylim=range(dat$vl,na.rm=TRUE),xaxt='n'))
+    withAs(xx=unique(dat[dat$pat==ii,c('time','vl')]),plot(xx$time/7,xx$vl,type='l',log=logAddY,yaxt='n',xlab='',ylab='',xlim=range(dat$time/7),ylim=range(dat$vl,na.rm=TRUE),xaxt='n'))
     logAxis(4,las=1)
     text(convertLineToUser(3,4),10^mean(par('usr')[3:4]),'Viral load',srt=-90,xpd=NA)
     par(new=TRUE)
-    withAs(xx=unique(dat[dat$pat==ii,c('time','CD4')]),plot(xx$time/7,xx$CD4,type='l',log='',yaxt='n',xlab='',ylab='',xlim=range(dat$time/7),ylim=range(dat$CD4,na.rm=TRUE),xaxt='n',col='red'))
+    withAs(xx=unique(dat[dat$pat==ii,c('time','CD4')]),plot(xx$time/7,xx$CD4,type='l',log=logAdd,yaxt='n',xlab='',ylab='',xlim=range(dat$time/7),ylim=range(dat$CD4,na.rm=TRUE),xaxt='n',col='red'))
     axis(4,pretty(dat$CD4),mgp=c(6,5,4),las=1,col='red',col.axis='red')
     text(convertLineToUser(3,4),10^mean(par('usr')[3:4]),'Viral load',srt=-90,xpd=NA)
     text(convertLineToUser(8,4),mean(par('usr')[3:4]),'CD4 count',srt=-90,xpd=NA,col='red')
   }
+  #changing scales
+  par(mfrow=c(3,2))
+  for(ii in unique(dat$pat)){
+    withAs(xx=dat[dat$pat==ii,],plot(xx$time/7,xx[,var],bg=patCols[xx$pat],pch=21,las=1,log=logAddY,yaxt='n',xlab='',ylab=lab,main=ii))
+    title(xlab='Time (weeks)',mgp=c(2,1,0))
+    logAxis(las=1)
+    thisDat<-dat[dat$pat==ii,]
+    par(new=TRUE)
+    withAs(xx=unique(dat[dat$pat==ii,c('time','vl')]),plot(xx$time/7,xx$vl,type='l',log=logAddY,yaxt='n',xlab='',ylab='',xaxt='n'))
+    logAxis(4,las=1)
+    text(convertLineToUser(3,4),10^mean(par('usr')[3:4]),'Viral load',srt=-90,xpd=NA)
+    par(new=TRUE)
+    withAs(xx=unique(dat[dat$pat==ii,c('time','CD4')]),plot(xx$time/7,xx$CD4,type='l',log=logAdd,yaxt='n',xlab='',ylab='',xaxt='n',col='red'))
+    axis(4,pretty(dat$CD4[dat$pat==ii]),mgp=c(6,5,4),las=1,col='red',col.axis='red')
+    text(convertLineToUser(3,4),10^mean(par('usr')[3:4]),'Viral load',srt=-90,xpd=NA)
+    text(convertLineToUser(8,4),mean(par('usr')[3:4]),'CD4 count',srt=-90,xpd=NA,col='red')
+  }
   #sapply(unique(dat$pat),function(xx)withAs(d=dat[dat$pat==xx,],lines(d$time/7,d$vl,col=patCols[xx],lwd=2)))
+}
+
+pdf('out/time_vs_all.pdf',width=8,height=8)
+  plot3vars('ic50','IFNa IC50',dat)
+  plot3vars('ic50','IFNa IC50',dat,logX=TRUE)
+dev.off()
+pdf('out/time_vs_all_beta.pdf',width=8,height=8)
+  plot3vars('beta','IFNb IC50',dat)
+  plot3vars('beta','IFNb IC50',dat,logX=TRUE)
+dev.off()
+pdf('out/time_vs_all_rep.pdf',width=8,height=8)
+  plot3vars('replication','Replicative capacity',dat)
+  plot3vars('replication','Replicative capacity',dat,logX=TRUE)
+dev.off()
+pdf('out/time_vs_all_vres.pdf',width=8,height=8)
+  plot3vars('vres','IFNa Vres',dat)
+  plot3vars('vres','IFNa Vres',dat,logX=TRUE)
+dev.off()
+pdf('out/time_vs_all_betaVres.pdf',width=8,height=8)
+  plot3vars('betaVres','IFNb Vres',dat)
+  plot3vars('betaVres','IFNb Vres',dat,logX=TRUE)
 dev.off()
 
 
@@ -158,11 +207,10 @@ dev.off()
 # regenerate old plot
 # show old plot + new data
 # new data plots with fits 
-# viral load plot (split by 35 weeks)
-# cd4 plot (split by 35 weeks)
+## viral load plot (split by 35 weeks)
+## cd4 plot (split by 35 weeks)
 # split out bulks "_Bulk"
 # plot Replicative over time
 # plot everything for beta
 
 # bayesian model 
-
