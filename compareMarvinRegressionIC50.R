84004e8ef40f83723cecdda1cb27318d677f0223
sherrillmix <sherrillmix@users.noreply.github.com>
2017-11-28 17:33:00 -0500

    Compare marvin and fit. Add 5 parameter fit

---
+++ compareMarvinRegressionIC50.R
@@ -0,0 +1,65 @@
+
+if(!exists('dat'))source('readNewData.R')
+if(!exists('ic50Fits'))source('plotIC50Curves.R')
+
+alphaIds<-lapply(rownames(ic50Fits),function(xx)which(dat$ID.for.Publications==xx))
+if(any(sapply(alphaIds,length)!=1))stop('Problem matching alphas')
+alphaIds<-unlist(alphaIds)
+betaIds<-lapply(rownames(ic50FitsBeta),function(xx)which(dat$ID.for.Publications==xx))
+if(any(sapply(betaIds,length)!=1))stop('Problem matching betas')
+betaIds<-unlist(betaIds)
+
+marvinAlpha<-dat[alphaIds,c('IFNa2.....Sept.2017.....IFNa2.Pooled.Donor.cells.IC50..pg.ml.','IFNa2.....Sept.2017.IFNa2.Pooled.Donor.Vres.at.5.5pg.ml...UT.')]
+colnames(marvinAlpha)<-c('ic50','vres')
+rownames(marvinAlpha)<-rownames(ic50Fits)
+
+marvinBeta<-dat[betaIds,c('IFNbeta..Pooled.Donor.cells.IC50..pg.ml.','IFNbeta..Pooled.Donor.Vres.at.4.400.pg.ml...UT.')]
+colnames(marvinBeta)<-c('ic50','vres')
+rownames(marvinBeta)<-rownames(ic50FitsBeta)
+
+pdf('out/marvin_vs_regression.pdf')
+  plot(marvinAlpha$ic50,ic50Fits[,'ic50'],xlab='Marvin IC50',ylab='Regression IC50',main='IFNa2',log='xy',xaxt='n',yaxt='n')
+  text(marvinAlpha$ic50,ic50Fits[,'ic50'],rownames(marvinAlpha),cex=.25,col='#FF000099')
+  logAxis(1);logAxis(2,las=1)
+  abline(0,1)
+  plot(marvinAlpha$vres,ic50Fits[,'percVres'],xlab='Marvin Vres',ylab='Regression Vres',main='IFNa2',log='xy',xaxt='n',yaxt='n')
+  text(marvinAlpha$vres,ic50Fits[,'percVres'],rownames(marvinAlpha),cex=.25,col='#FF000099')
+  logAxis(1);logAxis(2,las=1)
+  abline(0,1)
+  plot(marvinBeta$ic50,ic50FitsBeta[,'ic50'],xlab='Marvin IC50',ylab='Regression IC50',main='IFNb',log='xy',xaxt='n',yaxt='n')
+  text(marvinBeta$ic50,ic50FitsBeta[,'ic50'],rownames(marvinBeta),cex=.25,col='#FF000099')
+  logAxis(1);logAxis(2,las=1)
+  abline(0,1)
+  plot(marvinBeta$vres,ic50FitsBeta[,'percVres'],xlab='Marvin Vres',ylab='Regression Vres',main='IFNb',log='xy',xaxt='n',yaxt='n')
+  text(marvinBeta$vres,ic50FitsBeta[,'percVres'],rownames(marvinBeta),cex=.25,col='#FF000099')
+  logAxis(1);logAxis(2,las=1)
+  abline(0,1)
+dev.off()
+
+plotIfnsMarvin<-function(dilutes,concAlpha,xlab='',marvins){
+  ylims<-range(dilutes[,1:20],na.rm=TRUE)
+  par(mar=c(3.5,4.5,1.5,1),mfrow=c(1,2))
+  for(thisSample in unique(dilutes$sample)){
+    xx<-dilutes[dilutes$sample==thisSample,1:20]
+    thisMax<-mean(unlist(xx[,1:2]))
+    plotIfn(concAlpha,xx,thisSample,xlab=xlab,ylims=ylims)
+    abline(v=marvins[thisSample,'ic50'],col='red',lty=2)
+    abline(h=marvins[thisSample,'vres']*thisMax/100,col='red',lty=2)
+    text(10^par('usr')[1]*2,10^par('usr')[3]*1.3,sprintf('Marvin Vres=%s IC50=%s',format(marvins[thisSample,'vres'],digits=2),format(marvins[thisSample,'ic50'],digits=2)),adj=0)
+    plotIfn(concAlpha,xx,thisSample,xlab=xlab,log='x',scaleMax=TRUE)
+    abline(v=marvins[thisSample,'ic50'],col='red',lty=2)
+    abline(h=marvins[thisSample,'vres']/100,col='red',lty=2)
+    abline(h=0.5,lty=2)
+  }
+}
+pdf('out/allCurvesMarvin.pdf',width=10,height=5)
+  plotIfnsMarvin(dilutes,concAlpha,'IFNa2 concentration (pg/ml)',marvinAlpha)
+dev.off()
+pdf('out/allCurvesBetaMarvin.pdf',width=10,height=5)
+  plotIfnsMarvin(dilutesBeta,concBeta,'IFNb concentration (pg/ml)',marvinBeta)
+dev.off()
+
+
+ic50Diffs<-list('IFNa2 IC50'=log2(marvinAlpha$ic50/ic50Fits[,'ic50']),'IFNb IC50'=log2(marvinBeta$ic50/ic50FitsBeta[,'ic50']))
+vresDiffs<-list('IFNa2 Vres'=marvinAlpha$vres-ic50Fits[,'percVres'],'IFNb Vres'=marvinBeta$vres-ic50FitsBeta[,'percVres'])
+lapply(ic50Diffs,function(xx)names(which(abs(xx)>2)))
--- plotIC50Curves.R
+++ plotIC50Curves.R
@@ -3,6 +3,7 @@ library(dnar)
 
 concAlpha<-c(0, 0.001, 0.003, 0.008, 0.023, 0.068, 0.204, 0.611, 1.833, 5.5)
 concBeta<-c(0, 4.4E-05, 0.00044, 0.0044, 0.044, 0.44, 4.4, 44, 440, 4400)
+lowerLimit<-50
 
 #https://www.myassays.com/four-parameter-logistic-regression.html
 #http://stats.stackexchange.com/questions/61144/how-to-do-4-parametric-regression-for-elisa-data-in-r
@@ -10,16 +11,46 @@ concBeta<-c(0, 4.4E-05, 0.00044, 0.0044, 0.044, 0.44, 4.4, 44, 440, 4400)
 #b = Hill’s slope of the curve (i.e. this is related to the steepness of the curve at point c).
 #c = the point of inflection (i.e. the point on the S shaped curve halfway between a and d)
 #d = the maximum value that can be obtained (i.e. what happens at infinite dose)
-f<-function(B,x)exp(log(B[1]-B[4])-log(1+(x/B[3])^B[2]))+B[4]
-LS<-function(B,y,x){sum((log(y)-log(f(B,x)))^2)}
+#f<-function(B,x,transformX=function(x)x){if(any(x<=0))browser();exp(log(B[1]-B[4])-log(1+(transformX(x)/B[3])^B[2]))+B[4]}
+f5<-function(B,x,transformX=function(x)x){exp(log(B[1]-B[4])-B[5]*log(1+(transformX(x)/B[3])^B[2]))+B[4]}
+f4<-function(B,x,transformX=function(x)x){exp(log(B[1]-B[4])-log(1+(transformX(x)/B[3])^B[2]))+B[4]}
+LS<-function(B,y,x){sum((log(y)-log(f4(B,x)))^2)}
+LS5<-function(B,y,x){sum((log(y)-log(f5(B,x)))^2)}
+optIc50<-function(B)optim(1,function(x,B){(B[1]*.5-f5(B,x))^2},B=B,method='Brent',lower=10^-10,upper=10^2)$par
 fit4par<-function(concs,p24s){
+  p24s<-p24s[concs>0]
+  concs<-concs[concs>0]
   fits<-lapply(list(c(max(p24s),1,1,min(p24s)),c(max(p24s)*2,1,1,min(p24s)/10),c(mean(p24s),1,1,mean(p24s)),c(max(p24s),.01,1,1)),function(starts)suppressWarnings(nlminb(starts,LS,x=concs,y=p24s,lower=c(0,-Inf,-Inf,0))))
   #fit<-suppressWarnings(nlminb(c(max(p24s),1,1,min(p24s)),LS,x=concs,y=p24s,lower=c(0,-Inf,-Inf,0))$par)
   obj<-sapply(fits,'[[','objective')
   return(fits[[which.min(obj)]]$par)
 }
+fit5par<-function(concs,p24s){
+  p24s<-p24s[concs>0]
+  concs<-concs[concs>0]
+  fits<-lapply(list(c(max(p24s),1,1,min(p24s),1),c(max(p24s)*2,1,1,min(p24s)/10),c(mean(p24s),1,1,mean(p24s),1),c(max(p24s),.01,1,1),1),function(starts)suppressWarnings(nlminb(starts,LS5,x=concs,y=p24s,lower=c(0,-Inf,-Inf,0,-Inf))))
+  #fit<-suppressWarnings(nlminb(c(max(p24s),1,1,min(p24s)),LS,x=concs,y=p24s,lower=c(0,-Inf,-Inf,0))$par)
+  obj<-sapply(fits,'[[','objective')
+  return(fits[[which.min(obj)]]$par)
+}
 
-
+findVresIc50<-function(concAlpha,p24s){
+  origConcs<-rep(concAlpha,each=2*nrow(p24s))
+  p24s<-unlist(p24s)
+  fit<-fit5par(origConcs,p24s)
+  vres<-f5(fit,max(origConcs))
+  ic50<-optIc50(fit)
+  percVres<-vres/fit[1]*100
+  return(c('ic50'=ic50,'vres'=vres,'percVres'=percVres))
+}
+calcIc50s<-function(dilutes,concAlpha){
+  out<-do.call(rbind,lapply(unique(dilutes$sample),function(thisSample){
+    xx<-dilutes[dilutes$sample==thisSample,1:20]
+    findVresIc50(concAlpha,xx)
+  }))
+  rownames(out)<-unique(dilutes$sample)
+  return(out)
+}
 
 if(!exists('dilutes')){
   dilutions<-read.csv('VOA MM-cohort/dilutions.csv',stringsAsFactors=FALSE)
@@ -48,38 +79,51 @@ if(!exists('dilutes')){
     return(dilutes)
   }
   dilutes<-readIfns("VOA MM-cohort/IC50 Alpha for New patients BULK isol. .xls")
+  dilutes[,1:20][dilutes[,1:20]<lowerLimit]<-lowerLimit
   dilutes$dilution<-sapply(dilutes$sample,function(xx)if(xx %in% dilutions$sample[dilutions$ifn=='alpha']) dilutions[dilutions$ifn=='alpha'&dilutions$sample==xx,'dilution'] else 50)
   dilutes[,1:20]<-dilutes[,1:20]*dilutes$dilution/1000
   dilutesBeta<-readIfns("VOA MM-cohort/IC50 Beta for all BULK isolates .xlsx")
+  dilutesBeta[,1:20][dilutesBeta[,1:20]<lowerLimit]<-lowerLimit
   dilutesBeta$dilution<-sapply(dilutesBeta$sample,function(xx)if(xx %in% dilutions$sample[dilutions$ifn=='beta']) dilutions[dilutions$ifn=='beta'&dilutions$sample==xx,'dilution'] else 50)
   dilutesBeta[,1:20]<-dilutesBeta[,1:20]*dilutesBeta$dilution/1000
 }
 
-plotIfn<-function(concAlpha,p24s,main='',xlab='',ylims=range(p24s)){
-  origConcs<-concs<-rep(concAlpha,each=2*nrow(xx))
+plotIfn<-function(concAlpha,p24s,main='',xlab='',ylims=range(p24s),log='xy',scaleMax=FALSE){
+  origConcs<-concs<-rep(concAlpha,each=2*nrow(p24s))
+  if(scaleMax)maxs<-unlist(p24s[,1:2])
+  else maxs<-1
+  vresIc50<-findVresIc50(concAlpha,p24s)
+  p24s<-unlist(p24s)
   concs[concs==0]<-min(concs[concs>0])*.1
-  plot(concs,p24s,xlab=xlab,ylab='',log='xy',las=1,xaxt='n',main=main,bg=c('red','green','blue','orange'),pch=21,mgp=c(2.5,1,0),ylim=ylims,yaxt='n')
-  fit<-fit4par(origConcs,p24s)
-  fit2<-suppressWarnings(nlminb(c(max(p24s),1,1,min(p24s)),LS,x=concs[origConcs!=0],y=p24s[origConcs!=0],lower=c(0,-Inf,-Inf,0))$par)
+  fit<-fit5par(origConcs,p24s)
+  plot(concs,p24s/maxs,xlab=xlab,ylab='',log=log,las=1,xaxt='n',main=main,bg=c('red','blue','pink','cyan'),pch=21,mgp=c(2.5,1,0),ylim=ylims/mean(maxs),yaxt='n')
+  #fit2<-suppressWarnings(nlminb(c(max(p24s),1,1,min(p24s)),LS,x=concs[origConcs!=0],y=p24s[origConcs!=0],lower=c(0,-Inf,-Inf,0))$par)
   fakeConc<-10^seq(-10,10,.001)
-  fitLine<-f(fit,fakeConc)
-  lines(fakeConc,fitLine,col='#FF000066',lwd=3)
-  title(ylab='p24 concentration (ng/ml)',mgp=c(3.5,1,0))
-  logAxis(1,axisMin=min(origConcs[origConcs>0]))
-  logAxis(2,las=1)
+  fitLine<-f5(fit,fakeConc)
+  lines(fakeConc,fitLine/mean(maxs),col='#FF000066',lwd=3)
+  if(scaleMax)title(ylab='Proportion maximum p24',mgp=c(3.5,1,0))
+  else title(ylab='p24 concentration (ng/ml)',mgp=c(3.5,1,0))
+  if(grepl('y',log))logAxis(1,axisMin=min(origConcs[origConcs>0]))
+  else axis(1,pretty(par('usr')[1:2]))
+  if(grepl('y',log))logAxis(2,las=1)
+  else axis(2,pretty(par('usr')[3:4]),las=1)
   axis(1,min(origConcs[origConcs>0])*.1,0)
-  abline(h=c(fit[c(1,4)]),lty=3,col='#00000055')
-  abline(v=fit[3],lty=3,col='#00000055')
+  #abline(h=c(fit[c(1,4)]),lty=3,col='#00000055')
+  #abline(v=fit[3],lty=3,col='#00000055')
+  abline(v=vresIc50['ic50'],lty=3,col='#00000055')
   #if(fit[4]<par('usr')[4])
-  text(10^par('usr')[1]*2,10^par('usr')[3]*2,sprintf('Vres=%s IC50=%s',format(fit[4],digits=2),format(fit[3],digits=2)),adj=0)
+  vres<-f5(fit,max(origConcs))
+  abline(h=vresIc50['vres'],lty=3,col='#00000055')
+  text(10^par('usr')[1]*2,10^par('usr')[3]*2,sprintf('Vres=%s IC50=%s',format(vresIc50['percVres']*100,digits=2),format(vresIc50['ic50'],digits=2)),adj=0)
   #if(thisSample=='MM55.12.2B1 bulk')browser()
+  return(fit)
 }
 plotIfns<-function(dilutes,concAlpha,xlab=''){
   ylims<-range(dilutes[,1:20],na.rm=TRUE)
   par(mar=c(3.5,4.5,1.5,1))
   for(thisSample in unique(dilutes$sample)){
     xx<-dilutes[dilutes$sample==thisSample,1:20]
-    plotIfn(concAlpha,unlist(xx),thisSample,xlab=xlab,ylims=ylims)
+    plotIfn(concAlpha,xx,thisSample,xlab=xlab,ylims=ylims)
   }
 }
 plotDualIfns<-function(dilutes,dilutesBeta,concAlpha,concBeta){
@@ -91,8 +135,8 @@ plotDualIfns<-function(dilutes,dilutesBeta,concAlpha,concBeta){
   for(thisSample in uniqs){
     xx<-dilutes[dilutes$sample==thisSample,1:20]
     yy<-dilutesBeta[dilutesBeta$sample==thisSample,1:20]
-    plotIfn(concAlpha,unlist(xx),thisSample,xlab='IFNa2 concentration (pg/ml)',ylims=ylims)
-    plotIfn(concBeta,unlist(yy),thisSample,xlab='IFNb concentration (pg/ml)',ylims=ylims2)
+    plotIfn(concAlpha,xx,thisSample,xlab='IFNa2 concentration (pg/ml)',ylims=ylims)
+    plotIfn(concBeta,yy,thisSample,xlab='IFNb concentration (pg/ml)',ylims=ylims2)
   }
 }
 
@@ -107,3 +151,6 @@ dev.off()
 pdf('out/allCurvesCombo.pdf',width=10,height=5)
   plotDualIfns(dilutes,dilutesBeta,concAlpha,concBeta)
 dev.off()
+
+ic50Fits<-calcIc50s(dilutes,concAlpha)
+ic50FitsBeta<-calcIc50s(dilutesBeta,concBeta)
