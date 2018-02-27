eb171ba240a225b3cb0744d8ecb05e789270fd46
sherrillmix <sherrillmix@users.noreply.github.com>
2018-02-26 20:17:00 -0500

    Combine all meta data

--- readMeta.R
+++ readMeta.R
@@ -1,6 +1,63 @@
 library(dnar)
 library(xlsx)
 library(lubridate)
+fixDecimals<-function(xx,maxVisit=29){
+  splits<-strsplit(xx,'\\.')
+  visits<-as.numeric(sub('[oO]','0',sapply(splits,'[',2)))
+  pats<-sapply(splits,'[',1)
+  fixVisit<-ave(visits,pats,FUN=function(xx){
+    out<-xx
+    #catch crazy visits e.g. 70
+    out[out>maxVisit&out%%10]<-out[out>maxVisit&out%%10]/10
+    #catch too high values
+    potentialProbs<-which(xx%%10==0)
+    for(ii in rev(potentialProbs)){
+      if(ii==length(xx))break
+      #fix too high e.g. 20 instead of 2
+      if(any(out[(ii+1):length(out)]<out[ii]))out[ii]<-out[ii]/10
+    }
+    #catch too low value
+    probs<-out<cummax(out)
+    if(any(probs))out[probs]<-out[probs]*10
+    if(any(rank(out)!=1:length(out)))stop('Problem fixing visits: ',paste(xx,collapse=', '))
+    out
+  })
+  return(paste(pats,sprintf('%02d',fixVisit),sep='.'))
+}
+
+##MASTER META LIST##
+meta1<-read.csv('data/For Scot, Complete Master table AUG.2017_meta.csv',stringsAsFactors=FALSE)[,-1:-2]
+meta1<-meta1[,1:6]
+#meta1$id<-fillDown(meta1$ID)
+meta1$id<-sapply(strsplit(meta1$Time.Points,'\\.'),'[',1)
+meta1<-meta1[meta1$Time.Points!='Total number of sequences',]
+rownames(meta1)<-meta1$Time.Points
+
+meta2<-read.csv('data/New MM cohort patients.csv',stringsAsFactors=FALSE)
+#meta2<-meta2[meta2$Date!=''&!is.na(meta2$Date)&meta2$Date!='Date',]
+meta2<-meta2[meta2[,2]!='',]
+colnames(meta2)[1:2]<-c('ID','Time.Points')
+colnames(meta2)[colnames(meta2)=='Viral.load']<-'VL'
+colnames(meta2)[colnames(meta2)=='CD4.count']<-'CD4'
+meta2<-meta2[,1:6]
+meta2$id<-fillDown(meta2$ID)
+meta2$Time.Points<-sprintf('MM%s',meta2$Time.Points)
+tmp<-sub('\\.([0-9])$','.0\\1',mapply(function(xx,yy)sub('^MM[0-9]+',xx,yy),meta2$id,meta2$Time.Points))
+meta2$Time.Points<-tmp
+rownames(meta2)<-meta2$Time.Points
+
+meta<-rbind(meta1,meta2)
+meta$mm<-meta$id
+meta$Date[meta$Date=='07/0806']<-'07/08/2006'
+meta$Date[meta$Date==' 08/21/08']<-'08/21/2008'
+meta$rDate<-as_date(sapply(meta$Date,function(xx)ifelse(grepl('/[0-9]{4}',xx),mdy(xx),dmy(xx))))
+meta$vl<-as.numeric(gsub('[><,]','',meta$VL))
+meta$cd4<-as.numeric(meta$CD4)
+meta$visit<-sapply(strsplit(rownames(meta),'\\.'),'[',2)
+
+
+##CORRECTIONS and IFN###
+
 wb <- loadWorkbook("meta/EJ MM plasma cytokine data CORRECTED updated VL CD4 Jan2018.xlsx")
 
 rawMetas<-lapply(getSheets(wb),function(sheet){
@@ -51,10 +108,19 @@ targetCols<-names(colCounts)[colCounts==length(rawMetas)]
 ifnMeta<-do.call(rbind,lapply(rawMetas,'[',targetCols[orderIn(targetCols,colnames(rawMetas[[1]]))]))
 ifnMeta$ej<-sapply(strsplit(rownames(ifnMeta),' '),'[',1)
 ifnMeta$mm<-sapply(strsplit(rownames(ifnMeta),'[ .]'),'[',2)
-ifnMeta$visit<-sapply(strsplit(rownames(ifnMeta),'[ .]'),'[',3)
+ifnMeta$sample<-sub('[oO]','0',ifnMeta$sample)
+isVisitSample<-!is.na(ifnMeta$sample)&grepl('\\.',ifnMeta$sample)&!grepl('[a-zA-Z]$',ifnMeta$sample)
+ifnMeta$oldSample<-ifnMeta$sample
+ifnMeta$sample[isVisitSample]<-fixDecimals(ifnMeta$sample[isVisitSample])
+ifnMeta$visit<-sapply(strsplit(ifnMeta$sample,'[.]'),'[',2)
+ifnMeta<-ifnMeta[ifnMeta$mm %in% meta$mm,]
+tmp<-unique(ifnMeta[,c('mm','ej')])
+ejLookup<-structure(tmp$ej,.Names=tmp$mm)
+
 baseDate<-by(ifnMeta[,c('dfosx','rDate')],ifnMeta$mm,function(xx){zz<-table(xx$rDate-xx$dfosx);names(zz)[which.max(zz)]})
 
 
+## cell sorting ##
 sorts<-read.csv('meta/AFM MM data summary Jan2018.csv',stringsAsFactors=FALSE)
 colnames(sorts)[1]<-'patient'
 sorts$patient<-fillDown(sorts$patient)
@@ -64,3 +130,65 @@ colnames(sorts)<-newColnames
 sorts$ej<-sub(' ','',sapply(strsplit(sorts$Donor,'[.]'),'[',1))
 paste(sorts$ej,sorts$DFOSx) %in% paste(meta$ej,meta$dfosx)
 sorts$rDate<-as_date(sapply(sorts$Visit.Date,function(xx)ifelse(grepl('/',xx),mdy(xx),dmy(xx))))
+
+## Inflammation markers ##
+trans<-read.csv('meta/London Cohort inflammation markers ELISA data for Scott 07172017.csv',stringsAsFactors=FALSE)
+trans$sample<-fixDecimals(as.character(trans$Sample))
+
+## messy all combined list
+if(!exists('ejs'))source('readAllPats.R')
+
+
+
+## Joining ##
+
+##combine ifnMeta and meta
+#newMeta<-!paste(ifnMeta$rDate,ifnMeta$mm) %in% paste(meta$rDate,meta$mm)
+#minDiff<-apply(ifnMeta[newMeta,c('mm','rDate')],1,function(xx)min(c(Inf,abs(meta[meta$mm==xx[1],'rDate']-ymd(xx[2])))))
+#checked manually look all distinct
+#ifnMeta[newMeta,][minDiff<10,]
+newMeta<-!paste(meta$rDate,meta$mm) %in% paste(ifnMeta$rDate,ifnMeta$mm)
+minDiff<-apply(meta[newMeta,c('mm','rDate')],1,function(xx)min(c(Inf,abs(ifnMeta[ifnMeta$mm==xx[1],'rDate']-ymd(xx[2])))))
+#checked manually look distinct
+meta[newMeta,][minDiff<10,]
+meta$ej<-ejLookup[meta$mm]
+metaMerge<-meta
+metaMerge[,colnames(ifnMeta)[!colnames(ifnMeta) %in% colnames(meta)]]<-NA
+metaMerge[,c('sample','date','dfosx','viralLoad')]<-metaMerge[,c('Time.Points','Date','DFOSx','VL')]
+
+comboMeta<-rbind(ifnMeta,metaMerge[newMeta,colnames(ifnMeta)])
+
+#combine ejs
+thisEjs<-ejs[ejs$ej %in% comboMeta$ej&!grepl('[a-z]',ejs$date),]
+thisEjs$rDate<-as_date(sapply(thisEjs$date,function(xx)ifelse(grepl('/[0-9]{4}',xx),mdy(xx),dmy(xx))))
+newEj<-!paste(thisEjs$ej,thisEjs$rDate) %in% paste(comboMeta$ej,comboMeta$rDate)&!grepl('HAART',thisEjs$notes)
+thisEjs<-thisEjs[newEj,]
+thisEjs[,c('viralLoad','sample')]<-thisEjs[,c('vl','id')]
+thisEjs$visit<-trimws(sapply(strsplit(thisEjs$sample,'\\.'),'[',2))
+thisEjs$mm<-sapply(thisEjs$ej,function(xx)names(ejLookup)[ejLookup==xx])
+thisEjs<-thisEjs[!thisEjs$id %in% c('EJ 85.14','EJ85.11'),]
+minDiff<-apply(thisEjs[,c('mm','rDate')],1,function(xx)min(c(Inf,abs(comboMeta[comboMeta$mm==xx[1],'rDate']-ymd(xx[2])))))
+if(any(minDiff<10))stop('Close date in ejs')
+thisEjs[,colnames(comboMeta)[!colnames(comboMeta) %in% colnames(thisEjs)]]<-NA
+
+comboMeta<-rbind(comboMeta,thisEjs[,colnames(comboMeta)])
+
+##combine trans
+if(any(!trans$sample %in% sprintf('%s.%s',sub('EJ','',comboMeta$ej),comboMeta$visit)))stop('Found unknown sample in trans data')
+rownames(trans)<-trans$sample
+transCols<-colnames(trans)[!colnames(trans) %in% c('sample','Sample')]
+if(any(transCols %in% colnames(comboMeta)))stop('Duplicate column in trans')
+comboMeta[,transCols]<-trans[sprintf('%s.%s',sub('EJ','',comboMeta$ej),comboMeta$visit),transCols]
+
+##combine sort
+#first two samples are controls
+if(any(!paste(sorts$ej,sorts$rDate)[-1:-2] %in% paste(comboMeta$ej,comboMeta$rDate)))stop('Unknown sample in sorts')
+sortCols<-colnames(sorts)[grepl('BST|HLA|CD38|__',colnames(sorts))]
+if(any(sortCols %in% colnames(comboMeta)))stop('Duplicate column in trans')
+rownames(sorts)<-paste(sorts$ej,sorts$rDate)
+comboMeta[,sortCols]<-sorts[paste(comboMeta$ej,comboMeta$rDate),sortCols]
+
+write.csv(comboMeta,'out/combinedMeta.csv')
+
+
+
