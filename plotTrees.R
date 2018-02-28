74caf5d1e06de2a8887e5b8274f7f7ffc67c61bd
sherrillmix <sherrillmix@users.noreply.github.com>
2018-02-28 14:29:15 -0500

    Tweak for non poster display. Only midpoint mm23

--- plotTrees.R
+++ plotTrees.R
@@ -8,7 +8,7 @@ if(!exists('meta'))source('readNewData.R')
 pdf('out/trees.pdf',width=3,height=6)
 for(ii in list.files('trees','ph$',full.name=TRUE)){
 tree<-read.tree(ii)
-tree<-midpoint(tree)
+if(grepl('MM23',ii))tree<-midpoint(tree)
 times<-sapply(strsplit(tree$tip.label,'\\.'),'[[',2)
 ids<-sapply(strsplit(tree$tip.label,'\\.'),function(xx)paste(xx[1:2],collapse='.'))
 days<-as.numeric(meta[ids,'DFOSx'])
@@ -16,6 +16,6 @@ subject<-sapply(strsplit(tree$tip.label,'\\.'),'[[',1)
 timeCols<-rainbow.lab(length(unique(times)))
 names(timeCols)<-unique(times)[order(as.numeric(gsub('[^0-9]','',unique(times))))]
 #,layout='unrooted'
-print(ggtree(tree)+geom_tippoint(color=timeCols[times],size=2.5)+ggtitle(unique(subject))+theme(plot.title = element_text(hjust = .5,size=28,margin=margin(b=-10,unit='pt')))+geom_treescale(offset=-5,fontsize=5,x=.01,y=-8))
+print(ggtree(tree)+geom_tippoint(color=timeCols[times],show.legend=TRUE,size=1.5)+ggtitle(unique(subject))+theme(plot.title = element_text(hjust = .5,size=12,margin=margin(b=-10,unit='pt')))+geom_treescale(offset=-5,fontsize=3,x=.01,y=-8)+scale_color_manual('',breaks=names(timeCols),values=timeCols))
 }
 dev.off()
