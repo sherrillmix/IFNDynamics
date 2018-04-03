ae45260cec68d264b4c469801d4e97c4e0bfe976
sherrillmix <sherrillmix@users.noreply.github.com>
2018-04-02 21:20:35 -0400

    Change colors

--- readAllPats.R
+++ readAllPats.R
@@ -10,6 +10,7 @@ ejs<-ejs[!is.na(ejs$date)&ejs$date!=''&ejs$date!='Date'&ejs$date!='Sample date',
 ejs<-ejs[ejs$dfosx!='SEMEN '&!is.na(ejs$dfosx),]
 #ejs$pat<-fillDown(ifelse(grepl('EJ',ejs$id),sub('\\..*$','',ejs$id),NA))
 ejs$pat<-fillDown(gsub(' ','',sub('\\(.*','',sub('\\..*','',sub('EJ?','',ejs$id)))))
+ejs[ejs$pat=='6911','pat']<-'69'
 ejs$dfosx<-as.numeric(ejs$dfosx)
 ejs$cd4<-as.numeric(sub('[A-Za-z].*$','',sub('\\(.*','',sub('[?,]','',ejs$cd4))))
 ejs$vl<-as.numeric(sub('[A-Za-z].*$','',sub('N/D|\\?','',gsub('[,><] *','',ejs$vl))))
@@ -24,22 +25,24 @@ perEj$lastAbove800<-sapply(rownames(perEj),function(xx){id<-max(c(-Inf,which(ejs
 
 
 pats<-unique(ejs$pat)
-desiredPats<-c(4,23,42,52,89,92,101,104,108)
-pats<-pats[order(!pats %in% desiredPats,as.numeric(pats))]
-pdf('out/allPats.pdf',width=12,height=9)
+desiredPats<-c(23,42,89,92,104,67)
+#pats<-pats[order(!pats %in% desiredPats,as.numeric(pats))]
+pats<-c(desiredPats,pats[!pats %in% desiredPats])
+pdf('out/allPats.pdf',width=12,height=8)
   par(mfrow=c(5,5),mar=c(6,4,1,6))
   xlims<-range(ejs$dfosx,na.rm=TRUE)
   ylims<-range(ejs$cd4,na.rm=TRUE)
   ylims2<-range(ejs$vl,na.rm=TRUE)
   for(ii in pats){
     thisEj<-ejs[ejs$pat==ii&!is.na(ejs$dfosx),]
-    plot(thisEj$dfosx,thisEj$cd4,xlim=xlims,ylim=ylims,main=sprintf('EJ%s',ii),type='l',xlab='DFoSx',ylab='CD4 count',las=1)
-    points(thisEj$dfosx,thisEj$cd4,cex=.5)
+    withAs(thisEj=thisEj[!is.na(thisEj$cd4),],plot(thisEj$dfosx,thisEj$cd4,xlim=xlims,ylim=ylims,main=sprintf('EJ%s',ii),type='l',xlab='DFoSx',ylab='CD4 count',las=1))
+    points(thisEj$dfosx,thisEj$cd4,cex=.7)
     par(new=TRUE)
-    plot(thisEj$dfosx,thisEj$vl/1000,type='l',yaxt='n',xlab='',ylab='',xlim=xlims,ylim=ylims2/1000,xaxt='n',col='red')
-    points(thisEj$dfosx,thisEj$vl/1000,cex=.5,col='red')
-    axis(1,thisEj$dfosx,rep('',nrow(thisEj)),col='blue',tcl=-.25)
-    axis(4,pretty(ylims2/1000),col='red',las=1)
-    mtext('Viral load (x1000)',4,line=3,cex=.7)
+    withAs(thisEj=thisEj[!is.na(thisEj$vl),],plot(thisEj$dfosx,thisEj$vl,type='l',yaxt='n',xlab='',ylab='',xlim=xlims,ylim=ylims2,xaxt='n',col='red',log='y'))
+    logAxis(4,las=1,col.ticks='red',cex.axis=.9,col.axis='red')
+    points(thisEj$dfosx,thisEj$vl,cex=.7,col='red')
+    axis(1,thisEj$dfosx,rep('',nrow(thisEj)),tcl=-.25)
+    #axis(4,pretty(ylims2/1000),col='red',las=1)
+    mtext('Viral load',4,line=2.7,cex=.7,col='red')
   }
 dev.off()
