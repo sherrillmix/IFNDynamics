a1649aabda63781758804c26306757e6b6909b92
sherrillmix <sherrillmix@users.noreply.github.com>
2018-05-02 09:44:05 -0400

    Rename samples

--- plotGrowth.R
+++ plotGrowth.R
@@ -1,15 +1,18 @@
 library(dnar)
 library(vipor)
+barLookup<-read.csv('meta/barLookup.csv',stringsAsFactors=FALSE)
 marvinQVOAs<-c('MM23\\.18|MM34\\.25')
 grow<-read.csv('data/Plasma vs QVOA for Marvin.csv',stringsAsFactors=FALSE)
 grow$study<-ifelse(grepl(marvinQVOAs,grow$SampleID1),'London Cohort after ART',
   ifelse(grepl('^MM',grow$SampleID1),'London Cohort',
   ifelse(grepl('^CH',grow$SampleID1),'Transmission Pairs',
-  ifelse(grepl('^B[0-9]{3}_',grow$SampleID1),'QVOA Nussenzweig',
-  ifelse(grepl('^[0-9]{3}[-_]',grow$SampleID1),'QVOA Seaman',
+  ifelse(grepl('^B[0-9]{3}_',grow$SampleID1),'QVOA Lorenzi',
+  ifelse(grepl('^[0-9]{3}[-_]',grow$SampleID1),'QVOA Lorenzi (Seaman)',
   ifelse(grepl('^[0-9]{6}',grow$SampleID1),'QVOA Bar/Siliciano',
       NA))))))
 if(any(is.na(grow$study)))stop('Problem identifying study')
+grow$SampleID1<-sub('6220035K','622035K',grow$SampleID1)
+for(ii in 1:nrow(barLookup))grow$SampleID1<-sub(barLookup[ii,1],barLookup[ii,2],grow$SampleID1)
 tmp<-colnames(grow)[grep('[dD]ay',colnames(grow))]
 dayCols<-tapply(tmp,sub('\\.\\.ng.*','',sub('day','Day',sub('([0-9])\\.[0-9]$','\\1',tmp))),c)
 growMean<-do.call(cbind,lapply(dayCols,function(xx)apply(grow[,xx],1,mean)))
@@ -27,10 +30,10 @@ pdf('out/isolates_vs_qvoa.pdf',width=9,height=6)
     apply(growMean[selector,],1,function(xx)lines(days,xx,col='#00000033',lwd=2))
   }
 dev.off()
-pdf('out/isolates_vs_qvoa_day7.pdf',width=10,height=6,useDingbats=FALSE)
-  par(mar=c(7,3.5,.1,.1))
+pdf('out/isolates_vs_qvoa_day7.pdf',width=16,height=6,useDingbats=FALSE)
+  par(mar=c(5.2,3.5,.1,2))
   plot(1,1,type='n',xlab='',xlim=c(1,nrow(grow))+c(-1.5,1.5),ylim=range(cbind(growMin,growMax)),ylab='p24 (ng/ul)',las=1,xaxt='n',mgp=c(2.5,.9,0),xaxs='i')
-  axis(1,1:nrow(grow),grow$SampleID1,las=2,cex.axis=.6,mgp=c(3,.6,0))
+  slantAxis(1,1:nrow(grow),grow$SampleID1,las=2,cex=.6,location=.65,axisArgs=list(tcl=-.4),srt=-45)
   segments(1:nrow(grow),growMin[,'Day.7'],1:nrow(grow),growMax[,'Day.7'],col=studyCol[grow$study],lwd=2)
   points(rep(1:nrow(grow),length(dayCols[['Day.7']])),unlist(grow[,dayCols[['Day.7']]]),col=studyCol[grow$study],pch=c(1,1,2,2),cex=.7)
   points(1:nrow(grow),growMean[,'Day.7'],pch=21,bg=studyCol[grow$study],cex=1.5)
