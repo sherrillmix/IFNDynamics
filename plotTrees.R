b7e5da61fccd4d64351bbd65d1b19954a0664f7b
sherrillmix <sherrillmix@users.noreply.github.com>
2018-05-30 10:15:16 -0400

    Plot qvoa trees

--- plotTrees.R
+++ plotTrees.R
@@ -48,3 +48,39 @@ for(ii in list.files('trees','ph$',full.name=TRUE)){
   print(out)
 }
 dev.off()
+
+
+library(ape)
+library(grid)
+#pdf('out/voa_trees.pdf',width=3,height=6)
+for(ii in list.files('trees/qvoa','ph$',full.name=TRUE)){
+  message(ii)
+  tree<-ape::read.tree(ii)
+  days<-tree$tip.label
+  regex<-'^[^.]+\\.d0*([1-9][0-9]+)\\..*$'
+  isNum<-grepl(regex,days)
+  days[isNum]<-sub(regex,'\\1',days[isNum])
+  days[grepl('pbmc',days)]<-'PBMC'
+  days[grepl('VOA|MM23.18',days)]<-'VOA'
+  nDays<-max(as.numeric(days[isNum]))
+  #colorFunc<-colorRampPalette(c('black','red'),space='Lab')
+  colorFunc<-rainbow.lab
+  timeCols<-c(colorFunc(nDays),'grey','purple')
+  names(timeCols)<-c(1:nDays,'PBMC','VOA')
+  subject<-sub('\\..*','',basename(ii))
+  out<-ggtree(tree)+
+    #magic numbers to get dash aligned with tip
+    geom_tiplab(color=timeCols[days],size=7.5,label='-',vjust=.35)+
+    ggtitle(unique(subject))+
+    theme(plot.title = element_text(hjust = .5,size=12,margin=margin(b=-10,unit='pt')))+
+    geom_treescale(width=.01,offset=-5,fontsize=3,x=0,y=length(days)*.9)
+    #scale_color_gradientn(colours = rainbow(5))
+    #scale_fill_continuous(guide = guide_colorbar(direction = "horizontal"))
+    #scale_color_manual('',breaks=names(timeCols),values=timeCols)
+  vp<-viewport()
+  plot(1,1,type='n',xaxt='n',yaxt='n',xlab='',ylab='',bty='n')
+  print(out,vp=vp)
+  insetScale(c(1:nDays-.5,nDays+.5),timeCols[1:nDays],main='Days after onset of symptoms',insetPos = c(0.025, 0.3, 0.04, 0.95),at=c(1,500,1000,1500,2000))
+  #plot.phylo(tree,show.tip.label=FALSE,tip.color=timeCols[days])
+}
+#dev.off()
