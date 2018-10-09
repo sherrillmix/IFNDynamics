89d5951d2fcf9c6920ceb29c501e5b706de296dc
sherrillmix <sherrillmix@users.noreply.github.com>
2018-10-09 14:27:08 -0400

    Prepare for cure poster

--- rebound/readBeta.R
+++ rebound/readBeta.R
@@ -16,6 +16,8 @@ beta$source<-'stephanie'
 beta<-beta[colnames(beta)!='ic50',]
 
 mm<-read.csv('firstNadir.csv',stringsAsFactors=FALSE)
+mm<-mm[mm$isFirst|mm$qvoa|mm$isSix|mm$isLast|mm$isBetaNadir,]
+mm$isNadir<-mm$isBetaNadir
 mm$class<-ifelse(mm$qvoa,'QVOA',ifelse(mm$isNadir,'Nadir',ifelse(mm$isSix,'6 Month',ifelse(mm$isLast,'Last','Acute'))))
 mm$type<-'MM cohort'
 #mm$label<-sprintf('MM %s',ifelse(mm$class=='QVOA',sprintf('Outgrowth %s',mm$pat),mm$class))
@@ -33,6 +35,7 @@ pair$beta<-pair$IFNbeta.Pooled.Donor.cells.IC50..pg.ml
 #adjust for super IFN
 pair$beta<-pair$beta*6386
 pair[pair$class=='Recipient','class']<-'Acute'
+pair$pat<-pair$sample
 pair[,colnames(beta)[!colnames(beta) %in% colnames(pair)]]<-NA
 pair$source<-'shilpa'
 
@@ -62,11 +65,11 @@ classCols<-structure(rainbow.lab(length(uniqClass),alpha=ifelse(uniqClass %in% d
 ranges<-do.call(rbind,tapply(combo$beta,combo$label,range))
 rangeClass<-sapply(rownames(ranges),function(xx)combo[combo$label==xx,'class'][1])
 
-subs<-c('A06'='Patient 1','A09'='Patient 2','B106'='Patient 1','B199'='Patient 2','Recipient'='Recipients','Donor'='Donors','Lorenzi et al.'='Nussenzweig')
+subs<-c('A06'='Patient A06','A09'='Patient A09','B106'='Patient B106','B199'='Patient B199','Recipient'='Recipients','Donor'='Donors','Lorenzi et al.'='Outgrowth','S-30'='Patient S-30','BEAT-044'='Patient BEAT-044','BEAT-030'='Patient BEAT-030','MM23'='Patient MM23','MM34'='Patient MM34','ATI'='ATI Outgrowth')
 newNames<-sapply(names(pos),function(xx){for(ii in names(subs))xx<-sub(ii,subs[ii],xx);xx})
 pos<-pos+cumsum(names(pos)=='Acute')*.5+cumsum(names(pos)=='Acute Recipient')*.5 #+cumsum(names(pos)=='Lorenzi et al. B106')*.5+cumsum(names(pos)=='Outgrowth MM23')*.5
 subsets<-rev(list('mm'=names(pos)[!grepl('ATI|Rebound|Lorenzi',names(pos))],'lorenzi'=names(pos)[!grepl('ATI|Rebound',names(pos))],'prePost'=names(pos)[!grepl('Rebound',names(pos))],'noMont'=names(pos)[!grepl('BEAT|S-[0-9]',names(pos))],'all'=names(pos)))
-pdf('beta_qvoa_compare.pdf',width=8,height=5)
+pdf('beta_qvoa_compare.pdf',width=8,height=6)
 ylim=range(combo$beta)
 spread<-offsetX(log10(combo$beta),combo$label,width=.25)
 for(ii in names(subsets)){
@@ -75,7 +78,7 @@ for(ii in names(subsets)){
   if(ii=='all')marSpace<-0
   else marSpace<-marSpaces[ii]
   if(ii=='prePost')marSpace<-marSpaces['noMont']
-  par(mar=c(7.3,4+marSpace,.1,3))
+  par(mar=c(9.8,4+marSpace,.1,3))
   plot(pos[combo$label[selector]]+spread[selector],combo$beta[selector],log='y',yaxt='n',ylab='IFNb IC50 (pg/ml)',xlab='',xaxt='n',type='n',cex.lab=1.2,ylim=ylim)
   if(ii=='all')marSpaces<-sapply(subsets,function(xx)diff(convertUserToLine(1:0,2))*sum(!names(pos) %in% xx))
   slantAxis(1,pos[names(pos) %in% subsets[[ii]]],newNames[names(pos) %in% subsets[[ii]]],srt=-45)
@@ -90,4 +93,12 @@ for(ii in names(subsets)){
 }
 dev.off()
 
-
+combo$study<-ifelse(grepl('^A[0-9]+$',combo$pat),'VRC01',
+  ifelse(grepl('^B[0-9]+$',combo$pat),'Reservoir',
+    ifelse(grepl('^BEAT-',combo$pat),'BEAT',
+      ifelse(grepl('^MM[0-9]+',combo$pat),'MM',
+        ifelse(grepl('^S-[0-9]+',combo$pat),'ATI',
+          ifelse(grepl('^Donor|Recipient',combo$pat),'Transmission',
+            'UNKNOWN'
+))))))
+if(any(combo$study=='UNKNOWN'))stop('Unknown study')
--- rebound/readData.R
+++ rebound/readData.R
@@ -17,6 +17,7 @@ dat$source<-'stephanie'
 
 #Add Shilpa acute vs chronic, add Marvin nadir/first, add Marvin QVOA
 mm<-read.csv('firstNadir.csv',stringsAsFactors=FALSE)
+mm<-mm[mm$isFirst|mm$qvoa|mm$isSix|mm$isLast|mm$isNadir,]
 mm$class<-ifelse(mm$qvoa,'QVOA',ifelse(mm$isNadir,'Nadir',ifelse(mm$isSix,'6 Month',ifelse(mm$isLast,'Last','Acute'))))
 mm$type<-'MM cohort'
 #mm$label<-sprintf('MM %s',ifelse(mm$class=='QVOA',sprintf('Outgrowth %s',mm$pat),mm$class))
@@ -33,6 +34,7 @@ pair$type<-'CHAVI cohort'
 pair$label<-sprintf('%s %s',ifelse(pair$class=='Donor','Chronic','Acute'),pair$class)
 pair$ic50<-pair$IFNa2.Pooled.Donor.cells.IC50..pg..ml
 pair[pair$class=='Recipient','class']<-'Acute'
+pair$pat<-pair$sample
 pair[,colnames(dat)[!colnames(dat) %in% colnames(pair)]]<-NA
 pair$source<-'shilpa'
 
@@ -70,11 +72,11 @@ ranges<-do.call(rbind,tapply(combo$ic50,combo$label,range))
 rangeClass<-sapply(rownames(ranges),function(xx)combo[combo$label==xx,'class'][1])
 
 
-subs<-c('A06'='Patient 1','A09'='Patient 2','B106'='Patient 1','B199'='Patient 2','Recipient'='Recipients','Donor'='Donors','Lorenzi et al.'='Nussenzweig')
+subs<-c('A06'='Patient A06','A09'='Patient A09','B106'='Patient B106','B199'='Patient B199','Recipient'='Recipients','Donor'='Donors','Lorenzi et al.'='Outgrowth','S-30'='Patient S-30','BEAT-044'='Patient BEAT-044','BEAT-030'='Patient BEAT-030','MM23'='Patient MM23','MM34'='Patient MM34','ATI'='ATI Outgrowth')
 newNames<-sapply(names(pos),function(xx){for(ii in names(subs))xx<-sub(ii,subs[ii],xx);xx})
 pos<-pos+cumsum(names(pos)=='Acute')*.5+cumsum(names(pos)=='Acute Recipient')*.5 #+cumsum(names(pos)=='Lorenzi et al. B106')*.5+cumsum(names(pos)=='Outgrowth MM23')*.5
 subsets<-rev(list('mm'=names(pos)[!grepl('ATI|Rebound|Lorenzi',names(pos))],'lorenzi'=names(pos)[!grepl('ATI|Rebound',names(pos))],'prePost'=names(pos)[!grepl('Rebound',names(pos))],'noMont'=names(pos)[!grepl('BEAT|S-[0-9]',names(pos))],'all'=names(pos)))
-pdf('qvoa_compare.pdf',width=8,height=5)
+pdf('qvoa_compare.pdf',width=8,height=6)
 ylim=range(combo$ic50)
 spread<-offsetX(log10(combo$ic50),combo$label,width=.25)
 for(ii in names(subsets)){
@@ -83,7 +85,7 @@ for(ii in names(subsets)){
   if(ii=='all')marSpace<-0
   else marSpace<-marSpaces[ii]
   if(ii=='prePost')marSpace<-marSpaces['noMont']
-  par(mar=c(7.3,4+marSpace,.1,3))
+  par(mar=c(9.8,4+marSpace,.1,3))
   plot(pos[combo$label[selector]]+spread[selector],combo$ic50[selector],log='y',yaxt='n',ylab='IFNa2 IC50 (pg/ml)',xlab='',xaxt='n',type='n',cex.lab=1.2,ylim=ylim)
   if(ii=='all')marSpaces<-sapply(subsets,function(xx)diff(convertUserToLine(1:0,2))*sum(!names(pos) %in% xx))
   slantAxis(1,pos[names(pos) %in% subsets[[ii]]],newNames[names(pos) %in% subsets[[ii]]],srt=-45)
@@ -132,5 +134,33 @@ for(ii in names(controls)){
   #slantAxis(1,pos,names(pos))
   axis(1,pos,names(pos),las=1)
   }
+}
+dev.off()
+
+combo$study<-ifelse(grepl('^A[0-9]+$',combo$pat),'VRC01',
+  ifelse(grepl('^B[0-9]+$',combo$pat),'Reservoir',
+    ifelse(grepl('^BEAT-',combo$pat),'BEAT',
+      ifelse(grepl('^MM[0-9]+',combo$pat),'MM',
+        ifelse(grepl('^S-[0-9]+',combo$pat),'ATI',
+          ifelse(grepl('^Donor|Recipient',combo$pat),'Transmission',
+            'UNKNOWN'
+))))))
+if(any(combo$study=='UNKNOWN'))stop('Unknown study')
+
+
+spread<-offsetX(log10(combo$ic50),combo$label,width=.4,varwidth=TRUE)
+pdf('studies.pdf',height=2.5,width=6)
+for(ii in sort(unique(combo$study))){
+  message(ii)
+  selector<-combo$study==ii
+  par(mar=c(3,4,.1,.1))
+  pos<-structure(1:length(unique(combo$label[selector])),.Names=unique(combo$label[selector][orderIn(combo$label[selector],ordering)]))
+  plot(pos[combo$label[selector]]+spread[selector],combo$ic50[selector],log='y',yaxt='n',ylab='IFNa2 IC50 (pg/ml)',xlab='',xaxt='n',type='n',cex.lab=1.2,ylim=ylim,xlim=c(.5,length(pos)+.5))
+  par(lheight=.75)
+  prettyNames<-sub(' MM','\nMM',sub('ATI ','ATI\n',sub('Rebound ','Rebound\n',sub('outgrowth ','outgrowth\n',sub('Outgrowth ','Outgrowth\n',newNames[names(pos)])))))
+  twoLines<-grepl('\n',prettyNames)
+  for(jj in 1:length(pos))axis(1,pos[jj],prettyNames[jj],mgp=c(3,0,0),padj=1)
+  logAxis(las=1)
+  points(pos[combo$label[selector]]+spread[selector],combo$ic50[selector],pch=21,bg=classCols[combo$class[selector]],col=ifelse(combo$class[selector] %in% deemphasize,'#00000066','#000000CC'),lwd=ifelse(combo$class[selector]=='Rebound',1.75,1.5),cex=2)
 }
 dev.off()
