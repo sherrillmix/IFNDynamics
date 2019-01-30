595737d4bf825d6d7a980e8aa20ea6e513265c58
sherrillmix <sherrillmix@users.noreply.github.com>
2019-01-30 16:11:35 -0500

    Check Env pos. Fix lims

--- analyzeCG.R
+++ analyzeCG.R
@@ -9,6 +9,8 @@ uniqPair<-sort(unique(envCheck$pair))
 pdf('env_cg_vs_ic50.pdf',width=10,height=10)
   thisDat<-envCheck
   spreadX<-vipor::offsetX(log(thisDat$alphaIc50),thisDat$lmCg,width=.2)
+  xlim<-range(envCheck$lmCg)
+  ylim<-range(envCheck$alphaIc50)
   withAs(xx=thisDat,plot(xx$lmCg+spreadX,xx$alphaIc50,log='y',main='All viral isolates',ylab='IFNa2 IC50',xlab='Number of CG in LM region of Env',xlim=xlim,ylim=ylim,yaxt='n',pch=21,bg='#FF000055',col='#00000033'))
   logAxis(las=1)
   thisMod<-lm(log(alphaIc50)~lmCg,thisDat)
@@ -17,8 +19,6 @@ pdf('env_cg_vs_ic50.pdf',width=10,height=10)
   lines(fakeX,predY[,'fit'])
   polygon(c(fakeX,rev(fakeX)),c(predY[,'lwr'],rev(predY[,'upr'])),col='#00000011',border=NA)
   par(mfrow=c(3,ceiling(length(uniqPair)/3)))
-  xlim<-range(envCheck$lmCg)
-  ylim<-range(envCheck$alphaIc50)
   for(ii in uniqPair){
     thisDat<-envCheck[envCheck$pair==ii,]
     spreadX<-vipor::offsetX(log(thisDat$alphaIc50),thisDat$lmCg,width=.2)
@@ -43,3 +43,9 @@ pdf('env_cg_vs_time.pdf',width=10,height=10)
     withAs(xx=thisDat,plot(xx$time,xx$lmCg+spreadX,main=ii,xlab='Days after onset of symptoms',ylab='Number of CG in LM region of Env',xlim=xlim,ylim=ylim,pch=21,bg='#FF000055',col='#00000033',las=1))
   }
 dev.off()
+
+
+library(dnar)
+arupPos<-noGap2Gap(out$EnvAA[grep('HXB2',out$id)],c(149,177))
+lapply(arupPos,function(xx)table(substring(out$EnvAA,xx,xx)))
+
