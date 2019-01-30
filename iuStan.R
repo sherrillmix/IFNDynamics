7d4ceacd2ef68fbf0b10621a08e59fe164239ecd
sherrillmix <sherrillmix@users.noreply.github.com>
2019-01-30 15:44:23 -0500

    Calculate ius by bayes

--- iuStan.R
+++ iuStan.R
@@ -39,14 +39,13 @@ iuCode<-'
     //background~normal(2,2);
   }
 '
-iuMod <- stan_model(model_code = iuCode)
+#iuMod <- stan_model(model_code = iuCode)
 
 iuCodeSimple<-'
   data {
     int<lower=0> nCount;
     int<lower=0> counts[nCount];
     vector<lower=0>[nCount] dilutions;
-    int maxCount;
     real background;
   }
   parameters {
@@ -104,7 +103,6 @@ simpleCountIU<-function(mod,counts,dilutions,background,chains=20,...){
     nCount=length(counts),
     counts=counts,
     dilutions=dilutions,
-    maxCount=2000,
     background=mean(background)
   )
   #,control=list(adapt_delta=.95,max_treedepth=15)
@@ -114,40 +112,7 @@ simpleCountIU<-function(mod,counts,dilutions,background,chains=20,...){
 
 
 
-print(system.time(fit<-withAs(xx=tit[!tit$red&tit$dextran=='Media'&tit$virus=='MM33.14.11B1'&!is.na(tit$virus),],simpleCountIU(iuModSimple,xx$n,xx$dil,tit[!tit$red&is.na(tit$dil),'n']))))
+#print(system.time(fit<-withAs(xx=tit[!tit$red&tit$dextran=='Media'&tit$virus=='MM33.14.11B1'&!is.na(tit$virus),],simpleCountIU(iuModSimple,xx$n,xx$dil,tit[!tit$red&is.na(tit$dil),'n']))))
 
-fit<-withAs(xx=tit[!tit$red&tit$dextran=='Dextran'&tit$virus=='YU2'&!is.na(tit$virus),],simpleCountIU(iuModSimple,xx$n,xx$dil,tit[!tit$red&is.na(tit$dil),'n']));mean(fit)
 
-fit<-withAs(tit=tit[!tit$red,],countIU(iuMod,tit$n,tit$dil,tit$virus,tit[grepl('Media',tit$virus)&!tit$red,'n'],tit$treatSpin,chains=40))
 
-fit2<-withAs(tit=tit[!tit$red&tit$virus %in% c('MM33.13.1D6','Media 1','Media 2'),],countIU(iuMod,tit$n,tit$dil,tit$virus,tit[grepl('Media',tit$virus)&!tit$red,'n'],tit$treatSpin,chains=40))
-
-fit3<-withAs(xx=tit[!tit$red&tit$virus %in% c('YU2'),],countIU(iuMod,xx$n,xx$dil,xx$virus,tit[grepl('Media',tit$virus)&!tit$red,'n'],xx$treatSpin,chains=40))
-#,'maxCount','background'
-print(fit,pars=c('baseIU'))
-
-rt<-read.csv('data/RTs 01.05.2018.csv',skip=1,stringsAsFactors=FALSE)
-rt$old<-suppressWarnings(as.numeric(rt$old.RT)
-rt$new<-suppressWarnings(as.numeric(rt$New.RT))
-
-rt2<-read.csv('data/20190109_virusSubset.csv',stringsAsFactors=FALSE)
-rt2<-rt2[!is.na(rt2$number),]
-rt2$old<-as.numeric(rt2$old.RT)
-rt2$new<-as.numeric(rt2$New.RT)
-
-lims<-range(c(rt$old,rt$new,rt2$old,rt2$new),na.rm=TRUE)
-pdf('out/oldNewRT.pdf',width=4,height=4)
-  par(mar=c(3.75,3.75,.2,.2))
-  dnar::withAs(rt=rt[grep('MM',rt$X.2),],plot(rt$old,rt$new,xlab='Old RT',ylab='New RT',log='xy',xlim=lims,ylim=lims,yaxt='n',xaxt='n',mgp=c(2.75,1,0)))
-  dnar::logAxis(las=1)
-  dnar::logAxis(1)
-  points(rt2$old,rt2$new,col='blue')
-  abline(0,1,lty=2)
-  fit<-lm(new~0+old,dat=log(rbind(rt[,c('old','new')],rt2[,c('old','new')])))
-  fakeDat<-data.frame(old=seq(log(lims[1]),log(lims[2]),length.out=1000))
-  pred<-predict(fit,fakeDat,interval='conf')
-  pred2<-predict(fit,fakeDat,interval='pred')
-  lines(exp(fakeDat[,'old']),exp(pred[,'fit']))
-  polygon(exp(c(fakeDat$old,rev(fakeDat$old))),exp(c(pred[,'lwr'],rev(pred[,'upr']))),col='#00000022',border=NA)
-  polygon(exp(c(fakeDat$old,rev(fakeDat$old))),exp(c(pred2[,'lwr'],rev(pred2[,'upr']))),col='#00000011',border=NA)
-dev.off()
