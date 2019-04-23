0f3ce38533808a462ebe2650a4de3c600495cfff
sherrillmix <sherrillmix@users.noreply.github.com>
2019-04-23 18:15:44 -0400

    Standardize sequence naming

--- readSeqs.R
+++ readSeqs.R
@@ -1,7 +1,15 @@
 if(!exists('mmLookup'))source('readNewData.R')
 
+hxb2<-read.fa('work/hxb2.fa')$seq
 cohortRegex<-paste(c(sub('EJ','UK',ejLookup),ejLookup,mmLookup),collapse='|')
 seqs<-dnar::readFaDir('seqs')
+corrections<-dnar::readFaDir('seqs/correct')
+corrections$name<-sub('_fixed','',corrections$name)
+rownames(corrections)<-corrections$name
+if(any(!corrections$name %in% seqs$name))stop('Unmatched correction name')
+corrections[seqs$name[seqs$name %in% corrections$name],'rev']<-levenR::leven(seqs[seqs$name %in% corrections$name,'seq'],dnar::revComp(corrections[seqs$name[seqs$name %in% corrections$name],'seq']),oneToOne=TRUE,substring2=TRUE)<levenR::leven(seqs[seqs$name %in% corrections$name,'seq'],corrections[seqs$name[seqs$name %in% corrections$name],'seq'],oneToOne=TRUE,substring2=TRUE)
+corrections$seq[corrections$rev]<-dnar::revComp(corrections$seq[corrections$rev])
+seqs[seqs$name %in% corrections$name,'seq']<-corrections[seqs$name[seqs$name %in% corrections$name],'seq'] 
 sampleRegex<-'^.*(UK|MM)([0-9]+)[.-]([0-9]+).*$'
 if(any(!grepl(sampleRegex,seqs$name)))stop('UK/MM not found in all seqs')
 seqs$inCohort<-grepl(cohortRegex,seqs$name)
@@ -44,16 +52,35 @@ inCohort$id<-ave(1:nrow(inCohort),inCohort$baseName,FUN=function(xx)1:length(xx)
 inCohort$newName<-sprintf('%s.%03d.01',inCohort$baseName,inCohort$id)
 if(any(table(inCohort$newName)>1))stop('Multiple sequences with same name')
 write.csv(inCohort[,c('name','isolate','newName','seq')],'out/newNames.csv',row.names=FALSE)
-write.fa(inCohort$newName,inCohort$seq,'out/newNames.fa')
+write.fa(inCohort$newName,inCohort$seq,'out/mmLongitudinalSequences.fa')
+
 #align on LANL with HMM
 refName<-'B.FR.83.HXB2_LAI_IIIB_BRU_K03455'
-aligned<-read.fa('work/alignHmm_2019-04-16/AlignSeq.nt.fasta.gz')
+aligned<-read.fa('work/alignHmm_2019-04-23/AlignSeq.nt.fasta.gz')
+aligned$full5<-nchar(degap(substring(aligned$seq,2000,4000)))>1000
+aligned$five<-nchar(degap(substring(aligned$seq,5000,6000)))>500
+aligned$env<-nchar(degap(substring(aligned$seq,7000,8000)))>500
+aligned$full3<-nchar(degap(substring(aligned$seq,10000,10500)))>300
+aligned$seqType<-NA
+aligned[aligned$full5&aligned$full3&aligned$five&aligned$env,'seqType']<-'full'
+aligned[!aligned$full5&aligned$full3&aligned$five&aligned$env,'seqType']<-'threeHalf'
+aligned[!aligned$full5&!aligned$full3&!aligned$five&aligned$env,'seqType']<-'env'
+firstNonGap<-regexpr('[^-]',aligned$seq)
+lastNonGap<-regexpr('[^-]-*$',aligned$seq)
 if(any(!inCohort$newName %in% aligned$name))stop('Missing name in alignment')
 if(any(!aligned$name %in% c(refName,inCohort$newName)))stop('New name found in alignment')
-pdf('out/aligned_seqs.pdf',width=20)
+pdf('out/aligned_seqs.pdf',width=20,height=10)
 dnaplotr::plotDNA(aligned$seq)
+dnaplotr::plotDNA(aligned$seq,groups=ifelse(is.na(aligned$seqType),'bad',aligned$seqType))
+withAs(aligned=aligned[is.na(aligned$seqType),],dnaplotr::plotDNA(aligned$seq,groups=apply(aligned[,c('full5','five','env','full3')],1,function(xx)paste(as.numeric(xx),collapse=''))))
+#abline(v=c(1000,5500,6500,10000,10500))
 dev.off()
-allProts<-readFaDir('work/alignHmm_2019-04-16','fasta.gz')
+dnaplotr::plotDNA(aligned$seq[aligned$name %in% inCohort[inCohort$name %in% corrections$name,'newName']])
+
+select<-aligned$name %in% c(refName,'MM15.PLAS.SGS.00037.004.01');dnaplotr::plotDNA(aligned[select,'seq'],groups=aligned[select,'name'])
+dnaplotr::plotDNA(substring(aligned[c(1,709),'seq'],10000),groups=aligned[c(1,709),'name'])
+
+allProts<-readFaDir('work/alignHmm_2019-04-23','fasta.gz')
 allProts<-allProts[!allProts$file %in% c('AlignSeq.nt.fasta.gz','LTR.nt.fasta.gz'),]
 allProts$aa<-suppressWarnings(dna2aa(dnar::degap(allProts$seq)))
 allProts$prot<-sub('\\..*','',allProts$file)
@@ -89,4 +116,40 @@ findFrameShift(probs[6,'seq'],hxb2Seq[probs$prot][6])
 #substring(probEnv$seq[1:2],1225,1250)
 
 
+inCohort$nNt<-nchar(dnar::degap(inCohort$seq))
+hist(inCohort$nNt,breaks=200)
+bad<-list(
+  'shorter2000'=inCohort[inCohort$nNt<2000,'newName'],
+  'around4000'=inCohort[inCohort$nNt>3800&inCohort$nNt<4200,'newName'],
+  '6to8000'=inCohort[inCohort$nNt>6000&inCohort$nNt<8200,'newName']
+)
 
+pdf('out/problem_seqs.pdf',width=20)
+par(mar=c(4,3,1,12))
+#select<-(!aligned$full&!aligned$five&!aligned$env)|aligned$name==refName;dnaplotr::plotDNA(aligned[select,'seq'],groups=aligned[select,'name'])
+#select<-(aligned$full&aligned$five&!aligned$env)|aligned$name==refName;dnaplotr::plotDNA(aligned[select,'seq'],groups=aligned[select,'name'])
+#select<-(!aligned$full&aligned$five&!aligned$env)|aligned$name==refName;dnaplotr::plotDNA(aligned[select,'seq'],groups=aligned[select,'name'])
+#dnaplotr::plotDNA(aligned[c(1,709),'seq'],groups=aligned[c(1,709),'name'])
+#dnaplotr::plotDNA(aligned$seq[aligned$name %in% unlist(bad)],groups=aligned$name[aligned$name %in% unlist(bad)])
+dnaplotr::plotDNA(aligned$seq[lastNonGap>=lastNonGap[1]][c(1,2)])
+dev.off()
+
+badSelect<-(is.na(aligned$seqType)&!grepl('SGS',aligned$name))|lastNonGap>=lastNonGap[1]+10
+zz<-aligned[badSelect,c('name','full5','five','env','full3','seq')]
+zz$oldName<-sapply(zz$name,function(xx)inCohort[inCohort$newName==xx,'name'])
+zz$reason<-ifelse(!zz$five,'Missing upstream',ifelse(!zz$env,'Missing env',ifelse(!zz$full3,'Missing downstream',ifelse(lastNonGap[badSelect]>=lastNonGap[1]+10,"Extra on 3' end",NA))))
+write.csv(zz[,c('oldName','reason')],'out/weirdCoverage.csv',row.names=FALSE)
+
+pdf('out/problem_seqs.pdf',width=22)
+par(mar=c(4,3,1,15))
+#select<-(!aligned$full&!aligned$five&!aligned$env)|aligned$name==refName;dnaplotr::plotDNA(aligned[select,'seq'],groups=aligned[select,'name'])
+#select<-(aligned$full&aligned$five&!aligned$env)|aligned$name==refName;dnaplotr::plotDNA(aligned[select,'seq'],groups=aligned[select,'name'])
+#select<-(!aligned$full&aligned$five&!aligned$env)|aligned$name==refName;dnaplotr::plotDNA(aligned[select,'seq'],groups=aligned[select,'name'])
+#dnaplotr::plotDNA(aligned[c(1,709),'seq'],groups=aligned[c(1,709),'name'])
+#dnaplotr::plotDNA(aligned$seq[aligned$name %in% unlist(bad)],groups=aligned$name[aligned$name %in% unlist(bad)])
+dnaplotr::plotDNA(aligned$seq,groups=ifelse(is.na(aligned$seqType),'bad',aligned$seqType))
+dnaplotr::plotDNA(c(zz$seq,aligned[1,'seq']),groups=c(zz$oldName,'HXB2'))
+dev.off()
+
+
+any(!unlist(bad) %in% zz$name & !grepl('SGS',unlist(bad)))
