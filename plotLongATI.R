118619661d14eba4fe23a0374e1745a0d14cf91d
sherrillmix <sherrillmix@users.noreply.github.com>
2021-03-03 17:19:05 -0500

    Adjust plot

--- plotLongATI.R
+++ plotLongATI.R
@@ -13,9 +13,9 @@ patZeroPos<-structure((1:length(unique(manOut$pat)))*4-8,.Names=unique(manOut$pa
 pdf('out/longAti_Ic50.pdf',width=8,height=3.5)
 par(mfrow=c(1,2),mar=c(3.4,3.6,.1,.8))
 for(ifn in c('ic50.IFNa2','ic50.IFNb')){
-  plot(ifelse(manOut$day==0,patZeroPos[manOut$pat],manOut$day),manOut[,ifn],ylim=range(manOut[,ifn]),xlim=range(manOut$day[!is.na(manOut$day)]),log='y',yaxt='n',ylab=sub('ic50\\.','IC50 ',ifn),pch=21,xlab='Days after detectable rebound',bg=patCols[manOut$pat],cex=1.2,mgp=c(2.3,.8,0))
+  plot(ifelse(manOut$day==0,patZeroPos[manOut$pat],manOut$day),manOut[,ifn],ylim=range(manOut[,ifn]),xlim=range(manOut$day[!is.na(manOut$day)]),log='y',yaxt='n',ylab=sprintf('%s IC50',sub('ic50\\.','',ifn)),pch=21,xlab='Days after detectable rebound',bg=patCols[manOut$pat],cex=1.2,mgp=c(2.3,.8,0))
   dnar::logAxis(las=1)
-  legend('bottomleft',names(patCols),inset=0,pch=21,pt.bg=patCols,bty='n')
+  if(ifn=='ic50.IFNb')legend('topright',names(patCols),pch=21,pt.bg=patCols,bty='y',inset=.01,y.intersp=.9)
 }
 dev.off()
 
